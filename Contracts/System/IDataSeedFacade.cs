8d651baa8ee9eb3cb293fc717be2b703aec2ccaf
carlosmartin667 <46163584+carlosmartin667@users.noreply.github.com>
2022-01-31 14:38:03 -0300

    Initial commit

---
+++ Contracts/System/IDataSeedFacade.cs
@@ -0,0 +1,11 @@
+﻿using Havit.ComponentModel;
+
+namespace Havit.NewProjectTemplate.Contracts.System;
+
+[ApiContract]
+public interface IDataSeedFacade
+{
+	Task SeedDataProfileAsync(string profileName, CancellationToken cancellationToken = default);
+
+	Task<List<string>> GetDataSeedProfilesAsync(CancellationToken cancellationToken = default);
+}
